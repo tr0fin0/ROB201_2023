1c216cba49d4c69c19b108b38af4d4331f90d64c
tr0fin0 <guitrofino@gmail.com>
2023-04-16 15:16:37 +0200

    🚧 zero start angle

--- tp_rob201/worlds/my_world.py
+++ tp_rob201/worlds/my_world.py
@@ -34,6 +34,7 @@ class MyWorld(WorldAbstract):
 
         # POSITION OF THE ROBOT
         angle = random.uniform(-math.pi, math.pi) # math.pi/2.0 
-        self._robot_pos = ((439.0, 195), angle)
+        # self._robot_pos = ((439.0, 195), angle)
+        self._robot_pos = ((439.0, 195), 0)
         self._playground.add(robot, self._robot_pos)
 
