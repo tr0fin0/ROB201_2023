d33ea11b7e4d500285e7e4d6813360d95ef1eab5
tr0fin0 <guitrofino@gmail.com>
2023-04-16 15:14:13 +0200

    🚧 implement potential_field_control

--- tp_rob201/control.py
+++ tp_rob201/control.py
@@ -90,25 +90,87 @@ def potential_field_control(lidar, pose, goal):
     """
 
     # * TP2
+    atr_K = 1
+    atr_diff = goal[:2] - pose[:2]
+    atr_dist = np.sqrt(atr_diff[0]**2 + atr_diff[1]**2)
+
+    if atr_dist > DST_LIM:
+        atr_dF = atr_K * atr_diff / atr_dist
+
+    elif atr_dist > DST_MIN:
+        atr_dF = atr_K * atr_diff / DST_LIM
+
+    else:
+        command = {"forward": 0, "rotation": 0}
+        return command
+
+    atr_mag = np.sqrt(atr_dF[0]**2 + atr_dF[1]**2)
+    atr_ang = (np.arctan2(atr_dF[1], atr_dF[0]) - pose[2]) / (2*np.pi)
+
+
     distances = lidar.get_sensor_values()   # 
     angles = lidar.get_ray_angles()         # angles in radiums
 
-    K = 1
-    diff = goal[:2] - pose[:2]
-    dist = np.sqrt(diff[0]**2 + diff[1]**2)
+    # get array of bool's where the lidar found obstacules inside safe zone
+    isDangerous = distances < DST_SAFE
 
-    if dist > DIST_MIN:
-        dF = K * diff / dist
+    # create array of dangerous values
+    dstDanger = distances[isDangerous]
+    angDanger = angles[isDangerous]
 
-    elif dist > ROBOT_SIZE:
-        dF = K * diff / DIST_MIN
+    if len(dstDanger) != 0:
+        # get the closest obstacule values that is dangerous
+
+        minDst = np.min(dstDanger)
+        minIdx = np.argmin(dstDanger)
+        minAng = angDanger[minIdx]
+
+        x_0 = pose[0]
+        y_0 = pose[1]
+        angle_0 = pose[2]
+
+        obst = []
+        obst.append(minDst * np.cos(minAng + angle_0) + x_0)
+        obst.append(minDst * np.sin(minAng + angle_0) + y_0)
+    else:
+        print("errorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr")
+        obst = [0, 0]
+
+    rep_K = 0.45e6
+    rep_diff = obst[:2] - pose[:2]
+    rep_dist = np.sqrt(rep_diff[0]**2 + rep_diff[1]**2)
+
+    # if rep_dist > DST_LIM:
+    if rep_dist <= DST_SAFE:
+        rep_dF = rep_K / (rep_dist**3) * rep_diff * (1/rep_dist - 1/DST_SAFE)
 
     else:
-        return {"forward": 0, "rotation": 0}
+        rep_dF = [0, 0]
 
+    # rep_dF = np.linalg.norm(rep_dF)
+    rep_mag = np.sqrt(rep_dF[0]**2 + rep_dF[1]**2)
+    rep_ang = (np.arctan2(rep_dF[1], rep_dF[0]) - pose[2]) / (2*np.pi)
+
+    dF = (atr_dF - rep_dF) / (atr_mag + rep_mag)
     mag = np.sqrt(dF[0]**2 + dF[1]**2)
     ang = (np.arctan2(dF[1], dF[0]) - pose[2]) / (2*np.pi)
 
-    command = {"forward": 0.25*mag, "rotation": ang}
+    # mag = (atr_mag + )
+    # forward = 0.25*atr_mag + 0.25*rep_mag
+    # rotation = 0.85*atr_ang + 0.15*rep_ang
+
+    forward = 0.30*mag
+    rotation = 0.975*ang
+
+    # print(" ")
+    # print(f'{atr_dist:4.4f} {rep_dist:4.4f}')
+    # print(f"{[f'{x:+4.4f}' for x in atr_dF]} {[f'{x:+4.4f}' for x in rep_dF]}")
+    # print(f"{[f'{x:+4.4f}' for x in atr_diff]} {[f'{x:+4.4f}' for x in rep_diff]}")
+    # print(f"{atr_mag:+4.4f} {atr_ang:+4.4f} {rep_mag:+4.4f} {rep_ang:+4.4f}")
+    # print(" ")
+    # print(f"{[f'{x:+4.4f}' for x in dF]}")
+    # print(f"{forward:+4.4f} {rotation:+4.4f}")
+    # print("___"*35)
+    command = {"forward": forward, "rotation": rotation}
 
     return command
